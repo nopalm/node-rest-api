0a437ef5f5177c7bf59688a46849388ae7d6b641
nopalm <nopalmdev@gmail.com>
2021-05-17 20:35:54 +0700

    table product

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
 const ProductList = ({ products }) => {
+  let i = products.length;
   return (
     <div>
       <h1>Data Products</h1>
-      {products.map((product) => (
-        <div className="card" key={product._id}>
-          <div className="card-body">
-            <p className="card-text">product name : {product.productName}</p>
-            <p className="card-text">
-              product desc : {product.productDescription}
-            </p>
-          </div>
-        </div>
-      ))}
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Product Name</th>
+            <th scope="col">Product Desc</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products
+            .map((product) => (
+              <tr key={product._id}>
+                <th scope="row"> {i--} </th>
+                <td>{product.productName}</td>
+                <td>{product.productDescription}</td>
+                <td>
+                  <button className="btn btn-info btn-sm">Edit</button>
+                  <button className="btn btn-danger btn-sm">Delete</button>
+                </td>
+              </tr>
+            ))
+            .reverse()}
+        </tbody>
+      </table>
     </div>
   );
 };
